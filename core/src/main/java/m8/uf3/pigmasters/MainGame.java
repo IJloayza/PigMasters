3fa99020e2f19c479fad2a7395ca16f82426386d
AitorMonVi <2023_Aitor.Montejo@iticbcn.cat>
2025-04-30 19:36:10 +0200

    modificaciones en MainGame, FirstScreen y MainScreen

--- core/src/main/java/m8/uf3/pigmasters/MainGame.java
+++ core/src/main/java/m8/uf3/pigmasters/MainGame.java
@@ -2,12 +2,20 @@ package m8.uf3.pigmasters;
 
 import com.badlogic.gdx.Game;
 
+import m8.uf3.pigmasters.helpers.AssetManager;
 import m8.uf3.pigmasters.screens.FirstScreen;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class MainGame extends Game {
     @Override
     public void create() {
-        setScreen(new FirstScreen());
+        AssetManager.load();
+        setScreen(new FirstScreen(this));
+    }
+
+    @Override
+    public void dispose() {
+        super.dispose();
+        AssetManager.dispose();
     }
 }
--- core/src/main/java/m8/uf3/pigmasters/screens/FirstScreen.java
+++ core/src/main/java/m8/uf3/pigmasters/screens/FirstScreen.java
@@ -1,41 +1,90 @@
 package m8.uf3.pigmasters.screens;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.GlyphLayout;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
 
-/** First screen of the application. Displayed after the application is created. */
+import m8.uf3.pigmasters.MainGame;
+import m8.uf3.pigmasters.helpers.AssetManager;
+
 public class FirstScreen implements Screen {
-    @Override
-    public void show() {
-        // Prepare your screen here.
+
+    private MainGame game;
+
+    private Image background;
+
+    private Stage stage;
+    private Batch batch;
+    private OrthographicCamera camera;
+
+    private GlyphLayout textLayout;
+    private GlyphLayout textDescription;
+
+    public FirstScreen(MainGame game) {
+        this.game = game;
+
+        textLayout = new GlyphLayout();
+        textLayout.setText(AssetManager.font, "PigMasters");
+
+        textDescription = new GlyphLayout();
+        textDescription.setText(AssetManager.font, "Aprendre el camino del cerdo");
+
+        camera = new OrthographicCamera(1024, 768);
+        camera.setToOrtho(false);
+
+        StretchViewport viewport = new StretchViewport(1024, 768, camera);
+
+        stage = new Stage(viewport);
+
+        batch = stage.getBatch();
+
+        background = new Image(AssetManager.background);
+
+        stage.addActor(background);
     }
+
+    @Override
+    public void show() {}
 
     @Override
     public void render(float delta) {
-        // Draw your screen here. "delta" is the time since last render in seconds.
+        stage.draw();
+        stage.act(delta);
+
+        batch.begin();
+        AssetManager.font.draw(batch, textLayout, 1024 - textLayout.width/3,
+            668);
+
+        AssetManager.font.draw(batch, textDescription, 1024 - textLayout.width/5,
+            668 - 100);
+        batch.end();
+
+        /*
+        if (Gdx.input.isTouched()) {
+            game.setScreen(new MainScreen(game, batch, camera));
+            dispose();
+        }
+        */
     }
 
     @Override
-    public void resize(int width, int height) {
-        // Resize your screen here. The parameters represent the new window size.
-    }
+    public void resize(int width, int height) {}
 
     @Override
-    public void pause() {
-        // Invoked when your application is paused.
-    }
+    public void pause() {}
 
     @Override
-    public void resume() {
-        // Invoked when your application is resumed after pause.
-    }
+    public void resume() {}
 
     @Override
-    public void hide() {
-        // This method is called when another screen replaces this one.
-    }
+    public void hide() {}
 
     @Override
-    public void dispose() {
-        // Destroy screen's assets here.
-    }
+    public void dispose() {}
 }
