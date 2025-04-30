d26e27a522f02464405a84101f6b383901555ead
Santiago Muñoz <2023_santiago.munoz@iticbcn.cat>
2025-04-30 19:35:35 +0200

    Added fonts to assetmanager

--- core/src/main/java/m8/uf3/pigmasters/MainGame.java
+++ core/src/main/java/m8/uf3/pigmasters/MainGame.java
@@ -2,12 +2,16 @@ package m8.uf3.pigmasters;
 
 import com.badlogic.gdx.Game;
 
+import m8.uf3.pigmasters.helpers.AssetManager;
 import m8.uf3.pigmasters.screens.FirstScreen;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class MainGame extends Game {
     @Override
     public void create() {
+
+        AssetManager.load();
         setScreen(new FirstScreen());
+
     }
 }
--- core/src/main/java/m8/uf3/pigmasters/helpers/AssetManager.java
+++ core/src/main/java/m8/uf3/pigmasters/helpers/AssetManager.java
@@ -1,4 +1,38 @@
 package m8.uf3.pigmasters.helpers;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+
 public class AssetManager {
+
+    // Fonts
+    public static BitmapFont boldFont;
+    public static BitmapFont mediumFont;
+    public static BitmapFont smallFont;
+
+    // Textures
+
+
+    public static void load() {
+        // Fonts
+        boldFont = new BitmapFont(Gdx.files.internal("fonts/PixelHigh.ttf"));
+        mediumFont = new BitmapFont(Gdx.files.internal("fonts/PixelMiddle.ttf"));
+        smallFont = new BitmapFont(Gdx.files.internal("fonts/PixelSmall.ttf"));
+
+        boldFont.getData().setScale(0.6f);
+        mediumFont.getData().setScale(0.4f);
+        smallFont.getData().setScale(0.4f);
+
+        // Textures
+
+        // Sounds TODO
+
+    }
+
+    public static void dispose() {
+        boldFont.dispose();
+        mediumFont.dispose();
+        smallFont.dispose();
+    }
+
 }
